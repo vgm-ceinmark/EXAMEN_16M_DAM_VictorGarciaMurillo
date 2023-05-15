b0321725bb373794519e4171d5e10914354a6612
vgm-ceinmark <vgarciam@ceinmark.net>
2023-05-15 09:03:28 +0200

    Descripcion del Commit

---
+++ src/pkg/Saludo.java
@@ -0,0 +1,11 @@
+package pkg;
+
+public class Saludo {
+
+	public static void main(String[] args) {
+		System.out.println("Buenos días Ceinmark, soy Marta Gómez");
+		
+		System.out.print("Introduce un número para averiguar si es primo o no: ");
+	}
+	
+}
