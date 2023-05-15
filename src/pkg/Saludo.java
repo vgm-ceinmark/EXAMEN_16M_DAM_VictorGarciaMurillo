d29a6d8ada0947e66ecd584c9865dc9888c444ea
vgm-ceinmark <vgarciam@ceinmark.net>
2023-05-15 09:13:21 +0200

    Commit new branch Rama1 con primo

--- src/pkg/Saludo.java
+++ src/pkg/Saludo.java
@@ -1,11 +1,30 @@
 package pkg;
 
+import java.util.Scanner;
+
 public class Saludo {
 
 	public static void main(String[] args) {
+		Scanner ent = new Scanner(System.in);
+		int num=0;
+		boolean primo=true;
 		System.out.println("Buenos días Ceinmark, soy Marta Gómez");
 		
 		System.out.print("Introduce un número para averiguar si es primo o no: ");
+		num=ent.nextInt();
+		
+		for(int i=num-1;i>0;i--) {
+			if(num%i==0) {
+				primo = false;
+				break;
+			}
+		}
+		if(primo) {
+			System.out.println("Es primo");
+		} else {
+			System.out.println("No es primo");
+		}
+		
 	}
 	
 }
